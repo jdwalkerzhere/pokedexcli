365f8b8ea3d93b92bc277f0693c613284d288ca1
jdwalkerzhere <jdwalkerzhere@comcast.net>
2024-12-15 08:35:38 -0500

    command registry made

--- main.go
+++ main.go
@@ -7,7 +7,44 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var supportedCommands map[string]cliCommand
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	for cmdName, cmd := range supportedCommands {
+		fmt.Printf("%s: %s\n", cmdName, cmd.description)
+	}
+	return nil
+}
+
 func main() {
+	supportedCommands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
@@ -15,6 +52,12 @@ func main() {
 		scanner.Scan()
 		input := scanner.Text()
 		cleaned := strings.Fields(strings.ToLower(input))
-		fmt.Printf("Your command was: %s\n", cleaned[0])
+		command := cleaned[0]
+		cmd, ok := supportedCommands[command]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		cmd.callback()
 	}
 }
