813ef754667314ab47dfe63aec337429da94c6d7
jdwalkerzhere <jdwalkerzhere@comcast.net>
2024-12-16 11:24:50 -0500

    map and map back commands added

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/jdwalkerzhere/pokedexcli
+module pokedexcli
 
 go 1.22.3
--- main.go
+++ main.go
@@ -2,32 +2,84 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
+	"pokedexcli/api"
 	"strings"
 )
 
+type config struct {
+	Next     *string
+	Previous *string
+}
+
 type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
+	name          string
+	description   string
+	callback      func(*config) error
+	configuration *config
 }
 
 var supportedCommands map[string]cliCommand
 
-func commandExit() error {
+func commandExit(configuration *config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(configuration *config) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
 	fmt.Println()
 	for cmdName, cmd := range supportedCommands {
 		fmt.Printf("%s: %s\n", cmdName, cmd.description)
 	}
+	return nil
+}
+
+func commandMap(configuration *config) error {
+	res, err := http.Get(*configuration.Next)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	var response api.Response
+	if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
+		return err
+	}
+	for _, location := range response.Results {
+		fmt.Println(location.Name)
+	}
+	configuration.Next = &response.Next
+	configuration.Previous = &response.Prev
+	return nil
+}
+
+func commandMapB(configuration *config) error {
+	if configuration.Previous == nil || *configuration.Previous == "" {
+		fmt.Println("you're on the first page")
+		start := "https://pokeapi.co/api/v2/location-area"
+		configuration.Previous = &start
+	}
+	res, err := http.Get(*configuration.Previous)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	var response api.Response
+	if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
+		return err
+	}
+	for _, location := range response.Results {
+		fmt.Println(location.Name)
+	}
+	configuration.Next = &response.Next
+	configuration.Previous = &response.Prev
 	return nil
 }
 
@@ -43,9 +95,24 @@ func main() {
 			description: "Displays a help message",
 			callback:    commandHelp,
 		},
+		"map": {
+			name:        "map",
+			description: "Display the next 20 areas",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Display the previous 20 areas",
+			callback:    commandMapB,
+		},
 	}
 
 	scanner := bufio.NewScanner(os.Stdin)
+	locationAreaURL := "https://pokeapi.co/api/v2/location-area"
+	config := config{
+		Next:     &locationAreaURL,
+		Previous: nil,
+	}
 
 	for {
 		fmt.Print("Pokedex > ")
@@ -58,6 +125,9 @@ func main() {
 			fmt.Println("Unknown command")
 			continue
 		}
-		cmd.callback()
+		err := cmd.callback(&config)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 }
